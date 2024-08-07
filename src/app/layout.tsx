a47abfbcb93010a5d17af65ff6d84df52e7358a6
brijeshbase2brand <brijeshbase2brand@gmail.com>
2024-08-07 11:24:48 +0530

    cornical link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
           property="og:title"
           content="Turn Your Shopify Store into a Mobile App | AppCartify"
         />
+        <link rel="canonical" href="https://www.appcartify.com/" />
+
         <meta
           property="og:description"
           content="Transform your Shopify store into a mobile app with AppCartify. Boost sales and engagement with features like push notifications, secure checkout, and more."
